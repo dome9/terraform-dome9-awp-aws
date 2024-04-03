172cddf7e7baf2d780ce7ae7b70f78ba4da85f72
Benny Zemmour <bennyze@checkpoint.com>
2024-04-03 21:44:27 +0300

    add module outputs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,29 @@
-# output "awp_scan_mode" {
-#     description = "AWP scan mode <inAccount|saas>"
-#     value       = local.scan_mode
-# }
+output "cloud_account_id" {
+  description = "Cloud Guard account ID"
+  value       = resource.dome9_awp_aws_onboarding.awp_aws_onboarding_resource.cloud_account_id
+}
 
-# output "awp_cloudguard_account_id" {
-#     description = "AWS Cloud Account ID"
-#     value       = data.dome9_cloudaccount_aws.cloud_account.id
-# }
+output "agentless_protection_enabled" {
+  description = "AWP Status"
+  value       = resource.dome9_awp_aws_onboarding.awp_aws_onboarding_resource.agentless_protection_enabled
+}
 
-# output "awp_cross_account_role_name" {
-#     value = aws_iam_role.CloudGuardAWPCrossAccountRole.name
-# }
+output "should_update" {
+  description = "This module is out of date and should be updated to the latest version."
+  value       = resource.dome9_awp_aws_onboarding.awp_aws_onboarding_resource.should_update
+}
 
-# output "cross_account_role_external_id" {
-#     value = local.cross_account_role_external_id
-  
-# }
+output "awp_cross_account_role_arn" {
+  description = "Value of the cross account role arn that AWP assumes to scan the account"
+  value       = aws_iam_role.CloudGuardAWPCrossAccountRole.arn
+}
+
+output "missing_awp_private_network_regions" {
+  description = "List of regions in which AWP has issue to create virtual private network (VPC)"
+  value       = resource.dome9_awp_aws_onboarding.awp_aws_onboarding_resource.missing_awp_private_network_regions
+}
+
+output "account_issues" {
+  description = "Indicates if there are any issues with AWP in the account"
+  value       = resource.dome9_awp_aws_onboarding.awp_aws_onboarding_resource.account_issues
+}
